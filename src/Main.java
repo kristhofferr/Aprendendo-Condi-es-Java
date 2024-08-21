ca4822416a1a211d0255b3a3d7fbcec0b029a71d
kristhofferr <kristhoffer.souza@escola.pr.gov.br>
2024-08-21 20:46:25 -0300

    Criação do projeto Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Integer idadePessoa = 20;
+        String nomePessoa = "kristhoffer";
+
+        if(idadePessoa > 21){
+            System.out.println("é maior que  21");
+        }else{
+            System.out.println("Não é maior");
+        }
     }
+
 }
